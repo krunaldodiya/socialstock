0d2941537a75b4ebd44a02454ad4434317722108
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-09 13:43:17 +0530

    select country hotfix

--- src/screens/InitialScreen.tsx
+++ src/screens/InitialScreen.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {GET_INITIAL_SCREEN} from '../graphql/query';
 import getStackNavigator from '../libs/route';
+import screens from '../libs/screens';
 
 export const InitialScreen = () => {
   const {data} = useQuery(GET_INITIAL_SCREEN);
 
-  const AppNavigator = getStackNavigator(data?.initialScreen);
+  // const AppNavigator = getStackNavigator(data?.initialScreen);
+  const AppNavigator = getStackNavigator(screens.SelectCountry);
   const App = createAppContainer(AppNavigator);
 
   return <App />;
